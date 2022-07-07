1909102187e2638b1939ec3dcc21992897ac2ee7
Shazeedul <85868041+Shazeedul@users.noreply.github.com>
2022-07-07 13:36:46 +0600

    Add files via upload

---
+++ storage/framework/views/5185dc52ef71263c1a60dbab09ade781620d6554.php
@@ -0,0 +1,39 @@
+
+<?php $__env->startSection('content'); ?>
+ 
+<div class="card">
+  <div class="card-header">Customer Edit Page</div>
+  <div class="card-body">
+      
+      <form action="<?php echo e(route('customers.update',$customer->id)); ?>" method="post" enctype="multipart/form-data">
+        <?php echo csrf_field(); ?>
+        <?php echo method_field("PUT"); ?>
+        <input type="hidden" name="id" id="id" value="<?php echo e($customer->id); ?>" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="<?php echo e($customer->name); ?>" class="form-control"></br>
+        <label>Email</label></br>
+        <input type="email" name="email" id="email" value="<?php echo e($customer->email); ?>" class="form-control"></br>
+        <div class="form-group row">
+          <label for="colFormLabe2" class="col-sm-2 font-weight-bold col-form-label">
+              <h5> Status </h5>
+          </label>
+          <div class="form-group ">
+              <div class="form-check">
+                      <input class="form-check-input" type="radio" value="1" <?php echo e(($customer->status=="1")? "checked" : ""); ?> name="status">
+                      <label class="form-check-label">Active</label>
+              </div>
+              <div class="form-check">
+                      <input class="form-check-input" type="radio" value="0" <?php echo e(($customer->status=="0")? "checked" : ""); ?> name="status">
+                      <label class="form-check-label">Deactive</label>
+              </div>
+          </div>
+        </div></br>
+        
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('customers.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\admin\Documents\crud-app\resources\views/projects/edit.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c2bb22b0011bfbfed7bf6b2d5660497b6ffdcdbf.php
@@ -0,0 +1,30 @@
+
+<?php $__env->startSection('content'); ?>
+ 
+<div class="card">
+  <div class="card-header">Create Customer Page</div>
+  <div class="card-body">
+    <div class="pull-right">
+      <a class="btn btn-primary btn-sm" href="<?php echo e(route('customers.index')); ?>" title="Go back"> <i class="fas fa-backward "></i>Go Back </a>
+    </div>
+    <br>
+      <form action="<?php echo e(route('customers.store')); ?>" method="post" enctype="multipart/form-data">
+        <?php echo csrf_field(); ?>
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Email</label></br>
+        <input type="email" name="email" id="email" class="form-control"></br>
+        <label>Password</label></br>
+        <input type="password" name="password" id="password" class="form-control"></br>
+        <label>Confirm Password</label></br>
+        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"></br>
+        <label class="custom-file-label" for="image">Choose file</label></br>
+        <input type="file" class="custom-file-input" id="image" name="image"></br></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('customers.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\admin\Documents\crud-app\resources\views/projects/create.blade.php ENDPATH**/ ?>
